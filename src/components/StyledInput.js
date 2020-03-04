98af359e055c76da1d6d2ef8012939095d0d9321
Plactom <rrschiavofilho@gmail.com>
2020-03-03 22:13:36 -0300

    Ajustado o layout

--- src/components/StyledInput.js
+++ src/components/StyledInput.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components/native'
 
 export const StyledInput = styled.TextInput`
-    background-color: #fff;
     width: 100%;
-    border-radius: 5px;
-    height: 45px
+    background-color: #FFFFFF0F;
+    border-bottom-width: 1px;
+    border-bottom-color: #F67280;
+    height: 45px;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
 `
--- src/pages/petRegistration/index.js
+++ src/pages/petRegistration/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native'
+import { Text } from 'react-native'
 import { Background } from '../../components/Background'
 import { StyledInput } from '../../components/StyledInput'
 import { StyledContainer } from '../../components/StyledContainer'
@@ -7,7 +7,15 @@ import { StyledContainer } from '../../components/StyledContainer'
 export default function PetRegistration() {
   return (
     <Background>
-      <StyledContainer color='#6C5B7B' width='90%' height='200px' >
+      <StyledContainer color='#6C5B7B' width='90%' height='200px'>
+        <Text style={{
+          color: '#F67280', 
+          fontWeight: 'bold', 
+          fontSize: 15,
+          marginBottom: 5,
+        }}>
+          Nome
+        </Text>
         <StyledInput/>
       </StyledContainer>
     </Background>
