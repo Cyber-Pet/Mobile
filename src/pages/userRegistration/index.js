e4e09684310a6e6a86f7c9cb4641eac22003132a
Jaderson Jonas Silva <Jader_19_02@hotmail.com>
2020-03-10 19:51:10 -0300

    Tela de cadastro pronta (Sem api)

--- src/pages/userRegistration/index.js
+++ src/pages/userRegistration/index.js
@@ -2,12 +2,31 @@ import React from 'react';
 import { Background } from '../../components/Background'
 import { StyledInput } from '../../components/StyledInput'
 import { StyledContainer } from '../../components/StyledContainer'
+import { StyledText } from '../../components/StyledText';
+import { StyledSubmitButton } from '../../components/StyledSubmitButton';
 
 export default function UserRegistration() {
   return (
     <Background>
-      <StyledContainer color='#6C5B7B' width='90%' height='200px' >
-        <StyledInput/>
+      <StyledContainer color='#6C5B7B' width='90%' height='200px' marginTop='40%' >
+        <StyledText>
+          Nome
+        </StyledText>
+        <StyledInput style={{ marginBottom: 10 }}/>
+        <StyledText>
+          E-mail
+        </StyledText>
+        <StyledInput style={{ marginBottom: 10}}/>
+        <StyledText>
+          Senha
+        </StyledText>
+        <StyledInput style={{ marginBottom: 30}}/>
+        <StyledSubmitButton>
+          <StyledText>
+            Cadastrar
+          </StyledText>
+        </StyledSubmitButton>
+
       </StyledContainer>
     </Background>
   );
