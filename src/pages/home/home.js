4e80be9a8861eee2f5675f73002854cf23d9c73a
Jader <jaderson619@gmail.com>
2020-03-30 11:25:38 -0300

    Commit test

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Background } from '../../components/Background'
 
 
-export default function UserLogin() {
+export default function Home() {
   return (
     <Background>
-
+      
     </Background>
   )
 }
