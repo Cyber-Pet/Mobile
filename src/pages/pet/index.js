df58a1ba157656ad3057232454733afe6365d48e
Jaderson <jaderson619@gmail.com>
2020-04-21 19:30:11 -0300

    Adicionado edição de imagem na tela de pet

--- src/pages/pet/index.js
+++ src/pages/pet/index.js
@@ -33,7 +33,26 @@ export default function Pet({ navigation, route }) {
         id: null,
         scanned: true,
     })
-        
+    
+    async function _pickImage() {
+        try {
+            let result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                allowsEditing: true,
+                aspect: [4, 3],
+                quality: 0.1,
+                base64: true,
+            });
+            if (!result.cancelled) {
+                setValues({
+                    ...values,
+                    ["petImage"]: result.base64});
+            }
+        } catch (E) {
+            console.log(E);
+        }
+    };
+
     function toggleSwitch(id,valor) {
         let pos = -1;
         for (let i = 0; i < items.length; i++) {
@@ -87,13 +106,14 @@ export default function Pet({ navigation, route }) {
                 marginTop: '15%'
             }}>
                 <Avatar
+                    onPress={_pickImage}
                     activeOpacity={0.5}
                     rounded
                     size={120}
                     showEditButton
                     icon={{name: 'pets', type: 'material-design'}}
                     source={{
-                        uri: `data:image/png;base64,${petImage}`
+                        uri: `data:image/png;base64,${values.petImage}`
                     }}
                 />
                 <StyledInput placeholder={`${petName}`} style={{marginTop:'10%'}} height='30%' value={values.petName}></StyledInput>
